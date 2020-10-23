b356525c3d62f0420a30787b76cfac66a4ba11a2
Scott van Looy <scott@quul.net>
2020-10-23 08:55:41 +0200

    Update entrypoint.sh
    We want to do more than just the default in this github action - namely upload assets with gzip enabled and also set cache control to 1 year as each release generates a fresh url.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,10 @@ echo "$INPUT_SERVICE_KEY" | base64 --decode > "$HOME"/service_key.json
 
 gcloud auth activate-service-account --key-file="$HOME"/service_key.json --project "$INPUT_PROJECT"
 
-gsutil rsync -R "$INPUT_BUILD_FOLDER" gs://"$INPUT_BUCKET_NAME"
+gsutil -m cp -z "css,js,html,svg,json" -r . gs://"$INPUT_BUCKET_NAME"
 gsutil web set -m "$INPUT_HOME_PAGE_PATH" -e "$INPUT_ERROR_PAGE_PATH" gs://"$INPUT_BUCKET_NAME"
+gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.js
+gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.css
+gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.svg
+gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.woff
+gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.woff2
