1e66351e9480533dca86450b938bb7e605aaca0b
Scott van Looy <scott.vanlooy@foreverproject.co.uk>
2020-10-23 09:43:26 +0200

    Update entrypoint.sh
    Change cache control for everything added to the static bucket

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,4 @@ gcloud auth activate-service-account --key-file="$HOME"/service_key.json --proje
 cd "$INPUT_BUILD_FOLDER"
 gsutil -m cp -z "css,js,html,svg,json" -r . gs://"$INPUT_BUCKET_NAME"
 gsutil web set -m "$INPUT_HOME_PAGE_PATH" -e "$INPUT_ERROR_PAGE_PATH" gs://"$INPUT_BUCKET_NAME"
-gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.js
-gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.css
-gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.svg
-gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.woff
-gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.woff2
+gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*
